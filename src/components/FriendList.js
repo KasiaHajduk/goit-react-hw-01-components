387b185ce35dbf967f79d79645f876212ffdd332
KasiaHajduk <kasiahajduk2014@gmail.com>
2022-02-07 19:57:54 +0100

    0702-1957

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,14 +1,18 @@
+import styles from "./FriendList.module.css";
+import { FaCircle } from 'react-icons/fa';
+
+
 function FriendList(props) {
     const friends = props.friends;
     const viewFriends = friends.map((friend) =>
-        <li key={friend.id} className="item">
-            <span className="status"></span>
-            <img className="avatar" src={friend.avatar} alt="User avatar" width="48" />
-            <p className="name">{friend.name}</p>
+        <li key={friend.id} className={styles.item}>
+            <span className={styles.status}><FaCircle /></span>
+            <img className={styles.avatar} src={friend.avatar} alt="User avatar" width="48" />
+            <p className={styles.name}>{friend.name}</p>
         </li>
     );
 
-    return <ul>
+    return <ul className={styles.list}>
         {viewFriends}
     </ul>
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,39 +1,109 @@
 import styled from 'styled-components';
 
 const DivProfile = styled.div`
-  font-size: 1.5em;
+  background-color: #e7ecf2;
   text-align: center;
-  color: palevioletred;
-  background-color: red;
+  height: 370px;
+  padding-top: 20px;
 `;
+
+const DivDescription = styled.div`
+  background-color: #ffffff;
+  border: solid 1px #e2e8ef;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  width: 250px;
+  height: 260px;
+  text-align: center;
+  margin:0 auto;
+`;
+
+const ImgAvatar = styled.img`
+  margin-top: 30px;
+  width: 100px;
+  border: solid 1px #e2e8ef;
+  border-radius: 50px;
+`;
+
+const PName = styled.p`
+  color: #000000;
+  font-size: 16px;
+`;
+
+const PTag = styled.p`
+  color: #a1abb6;
+  font-size: 10px;
+`;
+
+const PLocation = styled.p`
+  color: #a1abb6;
+  font-size: 13px;
+`;
+
+const UlStats = styled.ul`
+  background-color: #f3f6f9;
+  margin: 0px;
+  padding: 0px;
+  border: solid 1px #e2e8ef;
+  border-top: 0px;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  width: 250px;
+  height: 70px;
+  //text-align: center;
+  margin:0 auto;
+  display: flex;
+`;
+
+const LiStats = styled.li`
+  margin: 0px;
+  padding: 0px;
+  border-left: solid 1px #e2e8ef;
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+  width: 83.3px;
+  vertical-align: middle;
+  padding-top: 15px; 
+`;
+
+const SpanLabel = styled.span`
+  color: #a1abb6;
+  font-size: 12px;
+`;
+
+const SpanQuantity = styled.span`
+  color: #000000;
+  font-size: 12px;
+`;
+
 
 function Profile(props) {
     const user = props.user;
     return <DivProfile>
-      <div className="description">
-        <img
+      <DivDescription>
+        <ImgAvatar
             src={user.avatar}
-            alt="User avatar"
-            className="avatar"
-        />
-        <p className="name">{user.username}</p>
-        <p className="tag">{user.tag}</p>
-        <p className="location">{user.location}</p>
-      </div>
-      <ul className="stats">
-        <li>
-            <span className="label">Followers</span>
-            <span className="quantity">{user.stats.followers}</span>
-        </li>
-        <li>
-            <span className="label">Views</span>
-            <span classname="quantity">{user.stats.views}</span>
-        </li>
-        <li>
-            <span className="label">Likes</span>
-            <span className="quantity">{user.stats.likes}</span>
-        </li>
-      </ul>
+            alt="User avatar">
+        </ImgAvatar>
+        <PName>{user.username}</PName>
+        <PTag>{user.tag}</PTag>
+        <PLocation>{user.location}</PLocation>
+      </DivDescription>
+      <UlStats>
+        <LiStats>
+            <SpanLabel>Followers</SpanLabel>
+            <SpanQuantity>{user.stats.followers}</SpanQuantity>
+        </LiStats>
+        <LiStats>
+            <SpanLabel>Views</SpanLabel>
+            <SpanQuantity>{user.stats.views}</SpanQuantity>
+        </LiStats>
+        <LiStats>
+            <SpanLabel>Likes</SpanLabel>
+            <SpanQuantity>{user.stats.likes}</SpanQuantity>
+        </LiStats>
+      </UlStats>
 </DivProfile>
 }
 
